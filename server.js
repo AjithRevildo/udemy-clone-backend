42215c683e6cd8d5b3f9885c483f31ecb18512dd
Ajith Revildo <ajithrevildo1999@gmail.com>
2022-03-24 06:40:28 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoDB from './db/mongoDB.js'
+
+import courseRoutes from './routes/courseRoutes.js'
+
+dotenv.config()
+
+mongoDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Revildo un app run avuthu daa')
+})
+
+app.use('/api/courses', courseRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT, 
+    console.log(`Server running on PORT: ${PORT}`)
+)
